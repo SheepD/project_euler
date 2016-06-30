5b61eced241149a4398a36c586017837edb8b46e
sheepd <maru_devera@outlook.com>
2016-06-30 14:58:23 +0800

    Euler Problem One
    * elixir: improve solution with better use of guard clauses
    * elixir: add unit test

--- elixir/problem1.exs
+++ elixir/problem1.exs
@@ -1,16 +1,22 @@
 defmodule Euler.ProblemOne do
-  def solve(n) do
-    sum_of_threes_and_fives(n)
+  def sum_of_threes_and_fives(n) when n < 3 do
+    0
   end
 
-  def sum_of_threes_and_fives(n) when n <= 1 do
-    if rem(n,3) < 1 || rem(n,5) < 1, do: n, else: 0
+  def sum_of_threes_and_fives(n) when rem(n,3) < 1 or rem(n,5) < 1 do
+    n + sum_of_threes_and_fives(n - 1)
   end
 
   def sum_of_threes_and_fives(n) do
-    sum = if rem(n,3) < 1 || rem(n,5) < 1, do: n, else: 0
-    sum + sum_of_threes_and_fives(n - 1)
+    sum_of_threes_and_fives(n - 1)
   end
 end
 
-IO.puts ProblemOne.solve(999)
+ExUnit.start
+defmodule ProblemOneTest do
+  use ExUnit.Case, async: true
+
+  test 'solution for problem one' do
+    assert Euler.ProblemOne.sum_of_threes_and_fives(999) == 233168
+  end
+end
